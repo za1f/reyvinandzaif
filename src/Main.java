150c31ec3bf2810a1c0115a33b45176744c566e2
BT_1N3_25 <sample@bths.edu>
2025-01-10 13:52:54 -0500

    eight commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("greetings");
+        System.out.println("adios chicos");
         System.out.println("Zaif aint single");
         System.out.println("Reyvin is single");
         System.out.println("Zaif loves taco bell");
