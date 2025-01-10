7872209dbe7fecdd3f6fbf3f5d13e4cf460530f1
BT_1N3_26 <zaifb@nycstudents.net>
2025-01-10 13:45:32 -0500

    hi guys

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("greetings");
         System.out.println("Zaif aint single");
+        System.out.println("Reyvin is single");
     }
 }
---
+++ src/ReyvinIsSingle.java
@@ -0,0 +1,3 @@
+public class ReyvinIsSingle {
+    String reyvin = "is single";
+}
